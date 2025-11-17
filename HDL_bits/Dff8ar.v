a59e32e550b7f45b5cc2724e91d816fc582fa2d8
Jeremy Chen <98017700+qchen4@users.noreply.github.com>
2025-11-17 17:51:09 +0000

    sequential circuits

---
+++ HDL_bits/Dff8ar.v
@@ -0,0 +1,15 @@
+module top_module (
+    input clk,
+    input areset,   // active high asynchronous reset
+    input [7:0] d,
+    output [7:0] q
+);
+    always @(posedge clk, posedge areset) 
+        if (areset)
+            q <= 'h00;
+        else
+            q <= d;
+ 
+        
+
+endmodule
