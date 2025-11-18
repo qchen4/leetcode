6925f376e807c63be392013fdada3b57758a229a
Jeremy Chen <98017700+qchen4@users.noreply.github.com>
2025-11-18 04:56:49 +0000

    daily leetcode

---
+++ leetcode/1437.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int prev = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 1) {
+                if (prev != -1 && (i - prev - 1 < k) ) 
+                {
+                    return false;
+                } else {
+                    prev = i;
+                }
+            }
+        }
+        return true;
+
+
+
+
+    }
+
+};
